ed7a8ce22d7475e84f8e4a467b9ebc95aead9832
Erik Hetzner <ehetzner@plos.org>
2015-05-18 21:18:17 -0700

    align README, commentary

--- totd.el
+++ totd.el
@@ -23,9 +23,21 @@
 
 ;;; Commentary:
 
-;;; Display a "tip of the day" at a given time of day.
+;; totd.el provides a daily morning "tip" - a command, bound to a key,
+;; selected at random from keymaps. To enable it, install via MELPA, then
+;; add:
 
-;;; Inspired by http://emacswiki.org/emacs/TipOfTheDay
+;; (totd-start)
+
+;; to your `~/.emacs.d/init.el` file.
+
+;; To display a tip right now, run `M-x totd`.
+
+;; By default, totd will display a random command that is bound to a key
+;; in `global-map`. To add maps (e.g. `org-mode-map`) to use a source for
+;; your daily tip, customize `totd-keymaps` by adding a symbol name.
+
+;; Inspired by http://emacswiki.org/emacs/TipOfTheDay
 
 ;;; Code:
 
